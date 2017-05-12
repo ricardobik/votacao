a118ce012dc83a7710d5beb2bab697f1b36006fc
Ricardo Rodrigo Teixeira <ricardobik@gmail.com>
2017-05-12 16:11:57 -0300

    Finishing create votacao

--- public/js/components/votacao.js
+++ public/js/components/votacao.js
@@ -1,199 +1,247 @@
-var qtdCandidato, qtdEleitor, qtdResponsavel = 0;
+var qtdCandidato = 0;
+var qtdEleitor = 0;
+var qtdResponsavel = 0;
 
 
 //Rules and Messages to Validate
 $("#votacao_form_partial").validate({
-	ignore: [],
-	debug: true,
-	rules: {
-		titulo: {
-			required: true
-		},
-		descricao: {
-			required: true
-		},
-		datainicio: {
-			required: true
-		},
-		datatermino: {
-			required: true
-		}
-	},
-	messages: {
-		titulo: {
-			required: "true"
-		},
-		descricao: {
-			required: "true"
-		},
-		datainicio: {
-			required: "true"
-		},
-		datatermino: {
-			required: "true"
-		}
-	}
+    ignore: [],
+    debug: true,
+    rules: {
+        titulo: {
+            required: true
+        },
+        descricao: {
+            required: true
+        },
+        datainicio: {
+            required: true
+        },
+        datatermino: {
+            required: true
+        }
+    },
+    messages: {
+        titulo: {
+            required: "true"
+        },
+        descricao: {
+            required: "true"
+        },
+        datainicio: {
+            required: "true"
+        },
+        datatermino: {
+            required: "true"
+        }
+    }
 });
 
 //Rules and Messages to Validate
 $("#votacao_form").validate({
-	ignore: [],
-	debug: true,
-	rules: {
-		titulo: {
-			required: true
-		},
-		descricao: {
-			required: true
-		},
-		datainicio: {
-			required: true
-		},
-		datatermino: {
-			required: true
-		}
-	},
-	messages: {
-		titulo: {
-			required: "true"
-		},
-		descricao: {
-			required: "true"
-		},
-		datainicio: {
-			required: "true"
-		},
-		datatermino: {
-			required: "true"
-		}
-	}
+    ignore: [],
+    debug: true,
+    rules: {
+        titulo: {
+            required: true
+        },
+        descricao: {
+            required: true
+        },
+        datainicio: {
+            required: true
+        },
+        datatermino: {
+            required: true
+        }
+    },
+    messages: {
+        titulo: {
+            required: "true"
+        },
+        descricao: {
+            required: "true"
+        },
+        datainicio: {
+            required: "true"
+        },
+        datatermino: {
+            required: "true"
+        }
+    }
 });
 
 //Create function
 function createVotacao(data) {
     console.log(data.candidato + data.responsavel + data.eleitor);
 
-	validator = $("#votacao_form_partial").validate();
+    validator = $("#votacao_form_partial").validate();
 
-	swal({
-			title: "Deseja Cadastrar essa votação?",
-			text: "A votação tem:\nCANDIDATOS: " + qtdCandidato + "\nELEITORES: " + qtdEleitor + "\nRESPONSÁVEIS: " + qtdResponsavel,
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: '#DD6B55',
-			confirmButtonText: 'Yes, I am sure!',
-			cancelButtonText: "No, cancel it!",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
-		function (isConfirm) {
-			if (!isConfirm) return;
-			//make AJAX request
-			if ($("#votacao_form_partial").valid()) {
-				$.ajax({
-					type: "POST",
-					url: urlApi + "votacao",
-					data: data,
-					dataType: "json",
+    swal({
+            title: "Deseja Cadastrar essa votação?",
+            text: "A votação tem:\nCANDIDATOS: " + qtdCandidato + "\nELEITORES: " + qtdEleitor + "\nRESPONSÁVEIS: " + qtdResponsavel,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: '#DD6B55',
+            confirmButtonText: 'Sim, Tenho certeza!',
+            cancelButtonText: "Não, Quero cancelar!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+        function (isConfirm) {
 
-					//if received a response from the server
-					success: function (response) {
-						slideOut("#cardFirst", 1350, -800, more);
-						resetForm($("#votacao_form_partial"));
-						validator.resetForm();
-					}
-				});
-			}
-		});
+            if (isConfirm) {
+
+                //make AJAX request
+                if ($("#votacao_form_partial").valid()) {
+                    $.ajax({
+                        type: "POST",
+                        url: urlApi + "votacao",
+                        data: data,
+                        dataType: "json",
+
+                        //if received a response from the server
+                        success: function (response) {
+                            slideOut("#cardFirst", 1350, -800, more);
+                            resetForm($("#votacao_form_partial"));
+                            validator.resetForm();
+
+                        }
+                    });
+                };
+
+                swal("Shortlisted!", "Votação cadastrada!", "success");
+
+            } else {
+                swal("Cancelled", "Você cancelou a ação", "error");
+            }
+        });
 
 
-	//Reload Material Form
-	Materialize.updateTextFields();
+    //    swal({
+    //                title: "Deseja Cadastrar essa votação?",
+    //                text: "A votação tem:\nCANDIDATOS: " + qtdCandidato + "\nELEITORES: " + qtdEleitor + "\nRESPONSÁVEIS: " + qtdResponsavel,
+    //                type: "warning",
+    //                showCancelButton: true,
+    //                confirmButtonColor: '#DD6B55',
+    //                confirmButtonText: 'Sim, Tenho certeza!',
+    //                cancelButtonText: "Não, Quero cancelar!",
+    //                closeOnConfirm: false,
+    //                closeOnCancel: false
+    //        },
+    //        function (isConfirm) {
+    //            if (!isConfirm) return;
+    //                //make AJAX request
+    //                if ($("#votacao_form_partial").valid()) {
+    //                    $.ajax({
+    //                        type: "POST",
+    //                        url: urlApi + "votacao",
+    //                        data: data,
+    //                        dataType: "json",
+    //    
+    //                        //if received a response from the server
+    //                        success: function (response) {
+    //                            slideOut("#cardFirst", 1350, -800, more);
+    //                            resetForm($("#votacao_form_partial"));
+    //                            validator.resetForm();
+    //    
+    //                        }
+    //                    });
+    //                swal("Shortlisted!", "Candidates are successfully shortlisted!", "success");
+    //
+    //            } else {
+    //                swal("Cancelled", "Your imaginary file is safe :)", "error");
+    //            }
+    //        });
+
+
+    //Reload Material Form
+    Materialize.updateTextFields();
 
 };
 
 
 function getVotacaoTable() {
 
-	table = $('table#table-votacao').DataTable({
-		ajax: {
-			url: urlApi + "votacao",
-			contentType: 'application/json; charset=UTF-8',
-			dataType: 'json'
-		},
-		columns: [{
-			data: "id"
+    table = $('table#table-votacao').DataTable({
+        ajax: {
+            url: urlApi + "votacao",
+            contentType: 'application/json; charset=UTF-8',
+            dataType: 'json'
+        },
+        columns: [{
+            data: "id"
                 }, {
-			data: "marcaid"
+            data: "marcaid"
                 }, {
-			data: "modeloid"
+            data: "modeloid"
                 }, {
-			data: "placa"
+            data: "placa"
                 }],
-		"columnDefs": [{
-			"width": "10%",
-			"targets": 0
+        "columnDefs": [{
+            "width": "10%",
+            "targets": 0
                 }, {
-			"width": "30%",
-			"targets": 1
+            "width": "30%",
+            "targets": 1
                 }, {
-			"width": "40%",
-			"targets": 2
+            "width": "40%",
+            "targets": 2
                 }, {
-			"width": "50%",
-			"targets": 3
+            "width": "50%",
+            "targets": 3
                 }],
-		select: true,
-		fixedColumns: true,
-		lengthChange: false,
-		pageLength: 5,
-		dom: 'lrti<"right"p>',
-		language: {
-			url: "../../doc/Portuguese-Brasil.json"
-		}
-	});
+        select: true,
+        fixedColumns: true,
+        lengthChange: false,
+        pageLength: 5,
+        dom: 'lrti<"right"p>',
+        language: {
+            url: "../../doc/Portuguese-Brasil.json"
+        }
+    });
 
 }
 
 //Fill motorista Modal to Edit
 function getVotacao(Id) {
 
-	$.ajax({
-		type: "GET",
-		url: urlApi + "votacao/" + Id,
+    $.ajax({
+        type: "GET",
+        url: urlApi + "votacao/" + Id,
 
-		dataType: "json",
+        dataType: "json",
 
-		//if received a response from the server
-		success: function (resp) {
+        //if received a response from the server
+        success: function (resp) {
 
-			$("#id").val(resp.data.id);
-			$("#tipo").val(resp.data.tipoid);
-			$("#placa").val(resp.data.placa);
+            $("#id").val(resp.data.id);
+            $("#tipo").val(resp.data.tipoid);
+            $("#placa").val(resp.data.placa);
 
-			getMarca(resp.data.marcaid, "select")
+            getMarca(resp.data.marcaid, "select")
 
-			getModelo(resp.data.modeloid, "select");
+            getModelo(resp.data.modeloid, "select");
 
-			getCombustiveis(resp.data.combustivelid);
+            getCombustiveis(resp.data.combustivelid);
 
-			getSetor(resp.data.setorid, "select");
+            getSetor(resp.data.setorid, "select");
 
-			$("#anofabricacao").val(resp.data.anofabricacao);
-			$("#combustivel").val(resp.data.combustivelid);
-			$("#renavam").val(resp.data.renavam);
-			$("#estadovotacao").val(resp.data.estadovotacao);
-			$("#info").val(resp.data.info);
+            $("#anofabricacao").val(resp.data.anofabricacao);
+            $("#combustivel").val(resp.data.combustivelid);
+            $("#renavam").val(resp.data.renavam);
+            $("#estadovotacao").val(resp.data.estadovotacao);
+            $("#info").val(resp.data.info);
 
-			//Reload Material Form
-			Materialize.updateTextFields();
+            //Reload Material Form
+            Materialize.updateTextFields();
 
-			//Load material dropbox
-			$('select').material_select();
+            //Load material dropbox
+            $('select').material_select();
 
-		}
+        }
 
-	});
+    });
 
 }
 
@@ -201,84 +249,82 @@ function getVotacao(Id) {
 function updateVotacao(id, data) {
 
 
-	//do AJAX request
-	$("#votacao_form").validate();
-	if ($("#votacao_form").valid()) {
+    //do AJAX request
+    $("#votacao_form").validate();
+    if ($("#votacao_form").valid()) {
 
-		swal({
-			title: "Confirmação",
-			text: "Deseja salvar as informações?",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#DD6B55",
-			confirmButtonText: "Salvar",
-			closeOnConfirm: false,
-			html: false
-		}, function () {
+        swal({
+            title: "Confirmação",
+            text: "Deseja salvar as informações?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Salvar",
+            closeOnConfirm: false,
+            html: false
+        }, function () {
 
-			$.ajax({
-				type: "PUT",
-				url: urlApi + "votacao/" + id,
-				data: data,
-				dataType: "json",
+            $.ajax({
+                type: "PUT",
+                url: urlApi + "votacao/" + id,
+                data: data,
+                dataType: "json",
 
-				//if received a response from the server
-				success: function (response) {
-					swal("Pronto!",
-						"As alterações foram salvas com sucesso.",
-						"success");
+                //if received a response from the server
+                success: function (response) {
+                    swal("Pronto!",
+                        "As alterações foram salvas com sucesso.",
+                        "success");
 
-					//Reload dataTable
-					$('#table-votacao').DataTable().ajax.reload();
-					$('#modal-edit').modal('close');
+                    //Reload dataTable
+                    $('#table-votacao').DataTable().ajax.reload();
+                    $('#modal-edit').modal('close');
 
-				}
+                }
 
-			});
+            });
 
-		});
+        });
 
-	}
-	//Reload Material Form
-	Materialize.updateTextFields();
+    }
+    //Reload Material Form
+    Materialize.updateTextFields();
 
 }
 
 //Delete function
 function deleteVotacao(id) {
-	swal({
-		title: "Tem certeza?",
-		text: "Esta ação excluirá o votacao!",
-		type: "warning",
-		showCancelButton: true,
-		confirmButtonClass: 'btn-danger',
-		confirmButtonText: 'Excluir',
-		cancelButtonText: "Cancelar",
-		closeOnConfirm: false,
-		closeOnCancel: false
-	}, function (isConfirm) {
-		if (isConfirm) {
+    swal({
+        title: "Tem certeza?",
+        text: "Esta ação excluirá o votacao!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: 'btn-danger',
+        confirmButtonText: 'Excluir',
+        cancelButtonText: "Cancelar",
+        closeOnConfirm: false,
+        closeOnCancel: false
+    }, function (isConfirm) {
+        if (isConfirm) {
 
-			$.ajax({
-				type: "DELETE",
-				url: urlApi + "votacao/" + id,
-				dataType: "json",
+            $.ajax({
+                type: "DELETE",
+                url: urlApi + "votacao/" + id,
+                dataType: "json",
 
-				//if received a response from the server
-				success: function (response) {
-					//Reload dataTable
-					$('#table-votacao').DataTable().ajax.reload();
-					$('#modal-edit').modal('close');
-				}
+                //if received a response from the server
+                success: function (response) {
+                    //Reload dataTable
+                    $('#table-votacao').DataTable().ajax.reload();
+                    $('#modal-edit').modal('close');
+                }
 
-			});
+            });
 
-			swal("Excluído!", "A votação foi excluída!", "success");
-		} else {
-			swal("Cancelado", "Nada foi modificado", "error");
-		}
-	});
+            swal("Excluído!", "A votação foi excluída!", "success");
+        } else {
+            swal("Cancelado", "Nada foi modificado", "error");
+        }
+    });
 
 }
-
-
